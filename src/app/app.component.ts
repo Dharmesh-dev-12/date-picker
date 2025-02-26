c6e5d82a553534ee6ad7bdfb7213fdeb5db85c4d
Dharmesh-dev-12 <dharmeshthanki74@gmail.com>
2025-02-26 12:58:21 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
+import { JsonPipe } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { NgbAlertModule, NgbDate, NgbDatepickerModule, NgbDateStruct, NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NgbDatepickerModule, NgbAlertModule, FormsModule, JsonPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'my-app';
+  
+  model!: NgbDate;
+  minDate: NgbDate;
+  maxDate: NgbDate;
+
+  constructor() {
+    // Set a minimum date (e.g., 1900-01-01) and a maximum date (e.g., 2099-12-31)
+    this.minDate = new NgbDate(1900, 1, 1);  // You can set the year range as required
+    this.maxDate = new NgbDate(3099, 12, 31); // Same here for the upper limit
+  }
+
 }
